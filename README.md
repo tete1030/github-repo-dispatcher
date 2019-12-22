0ecea05bb0c16736fbe2555a8368bf8d9c324f69
Texot <tete1030@gmail.com>
2019-12-22 22:55:07 +0800

    Add deployments support

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
-Github Repository Dispatcher
+Github Deployments and Repository Dispatch Trigger
 ============================================================================
 
-A Tampermonkey userscript for triggering Github repository dispatch event with one click
+A Tampermonkey userscript for triggering Github deployments and repository dispatch event with one click
 
-![Screenshot](imgs/btn.png)
-![Screenshot](imgs/type.png)
-![Screenshot](imgs/payload.png)
+![Screenshot](https://raw.githubusercontent.com/tete1030/github-repo-dispatcher/master/imgs/btn.png)
+![Screenshot](https://raw.githubusercontent.com/tete1030/github-repo-dispatcher/master/imgs/type.png)
+![Screenshot](https://raw.githubusercontent.com/tete1030/github-repo-dispatcher/master/imgs/payload.png)
 
 ## Prerequisite
 
@@ -15,14 +15,22 @@ Tampermonkey / Greasemonkey
 
 1. Install the script by copying or from [Greasefork](https://greasyfork.org/en/scripts/394032-github-repository-dispatch-trigger).
 2. Get github personal access token with repo permissions. Copy it.
-3. Open the repo you want to trigger repo dispatch event.
+3. Open the repo you want to trigger deployments or repo dispatch event.
+
+### For `repository_dispatch` event
 4. Click the "Repo Dispatch" button.
 5. At the first time, you will be asked to input your personal access token.
 6. Input your dispatch type (or any name you want. The name can be used to identify the event in Github Actions).
 7. Input your dispatch payload (optional, in `json`. E.g. `{"debug": true}`).
-8. Enjoy!
 
-## Documentations of `repository_dispatch` event
+### For `deployments` event
+4. Click the "Deploy" button.
+5. At the first time, you will be asked to input your personal access token.
+6. Input your task name (or any name you want. The name can be used to identify the event in Github Actions).
+7. Input the ref of your deploy, it can be branches/tags/commits
+8. Input your dispatch payload (optional, in `json`. E.g. `{"debug": true}`).
+
+## Documentations
 
 * [`repository_dispatch` in Github Actions](https://help.github.com/en/actions/automating-your-workflow-with-github-actions/events-that-trigger-workflows#external-events-repository_dispatch)
 * [Create a repository dispatch event](https://developer.github.com/v3/repos/#create-a-repository-dispatch-event)
--- userscript.js
+++ userscript.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name         Github Repository Dispatch Trigger
-// @version      0.1
-// @description  Add repository_dispatch trigger for Github Actions
+// @name         Github Deployments and Repository Dispatch Trigger
+// @version      0.2
+// @description  Trigger deployment or repository_dispatch event for Github Actions
 // @author       Texot
 // @namespace    https://github.com/tete1030/github-repo-dispatcher
 // @homepage     https://github.com/tete1030/github-repo-dispatcher
@@ -15,55 +15,100 @@
 (function() {
     'use strict';
 
-    $(document).ready(() => {
-        let btn = $("<a class='btn btn-sm'>Repo Dispatch</a>");
-        btn.on("click", () => {
-            let token = GM_getValue("token", "");
-            if (!token) {
-                token = window.prompt("Token");
-            }
-            if (!token) return;
-            GM_setValue("token", token);
-            let pathArray = window.location.pathname.split('/');
-            if (pathArray.length < 3) return;
-            let type = window.prompt("Type", GM_getValue("type", ""));
-            if (!type) return;
-            GM_setValue("type", type);
-            let payload = window.prompt("Client Payload", GM_getValue("payload", ""));
-            if (payload === null) return;
-            GM_setValue("payload", payload);
-            let data = {event_type: type};
-            try {
-                if (payload) data.client_payload = JSON.parse(payload);
-            } catch (e) {
-                window.alert("JSON parse failed: " + e.message);
-                return;
-            }
-            GM_xmlhttpRequest({
-                method: "POST",
-                url: "https://api.github.com/repos/" + pathArray[1] + "/" + pathArray[2] + "/dispatches",
-                headers: {"Authorization": "token " + token, "Accept": "application/vnd.github.everest-preview+json"},
-                data: JSON.stringify(data),
-                onload: (resp) => {
-                    if (resp.status == 204 || resp.status == 200) {
-                        console.log("Repo dispatch succeed: " + resp.responseText);
-                        window.alert("Repo dispatch succeed" + (resp.responseText?(": " + resp.responseText):""));
-                    } else {
-                        var message = resp.responseText;
-                        try {
-                            message = JSON.parse(message).message;
-                        } catch (e) {}
-                        console.log("Repo dispatch failed: " + resp.responseText);
-                        window.alert("Repo dispatch failed: " + message);
-                    }
-                },
-                onerror: (e) => {
-                    console.log("Repo dispatch failed: " + e);
-                    window.alert("Repo dispatch failed: " + e.message);
+    function ask_parameter(varid, prompt, save, allow_empty, skip) {
+        if (skip && GM_getValue(varid, null) !== null) return GM_getValue(varid);
+        let value = window.prompt(prompt, GM_getValue(varid, ""));
+        if (value===null) throw null;
+        if (!allow_empty && !value) {
+            window.alert("Value should not be empty");
+            throw null;
+        }
+        if (save) GM_setValue(varid, value);
+        return value;
+    }
+
+    function trigger_repo_dispatch() {
+        let pathArray = window.location.pathname.split('/');
+        if (pathArray.length < 3) return;
+        let token = ask_parameter("token", "Token", true, false);
+        let type = ask_parameter("repo_dis_type", "Type", true);
+        let payload = ask_parameter("repo_dis_payload", "Client Payload (in JSON)", true, true);
+        let data = {event_type: type};
+        try {
+            if (payload) data.client_payload = JSON.parse(payload);
+        } catch (e) {
+            window.alert("JSON parse failed: " + e.message);
+            return;
+        }
+        GM_xmlhttpRequest({
+            method: "POST",
+            url: "https://api.github.com/repos/" + pathArray[1] + "/" + pathArray[2] + "/dispatches",
+            headers: {"Authorization": "token " + token, "Accept": "application/vnd.github.everest-preview+json"},
+            data: JSON.stringify(data),
+            onload: (resp) => {
+                if (resp.status == 204 || resp.status == 200) {
+                    console.log("Repo dispatch succeed: " + resp.responseText);
+                    window.alert("Repo dispatch succeed" + (resp.responseText?(": " + resp.responseText):""));
+                } else {
+                    var message = resp.responseText;
+                    try {
+                        message = JSON.parse(message).message;
+                    } catch (e) {}
+                    console.log("Repo dispatch failed: " + resp.responseText);
+                    window.alert("Repo dispatch failed: " + message);
                 }
-            });
+            },
+            onerror: (e) => {
+                console.log("Repo dispatch failed: " + e);
+                window.alert("Repo dispatch failed: " + e.message);
+            }
         });
-        $(".pagehead-actions").eq(0).prepend($("<li></li>").append(btn));
+    }
+
+    function trigger_deployments() {
+        let pathArray = window.location.pathname.split('/');
+        if (pathArray.length < 3) return;
+        let token = ask_parameter("token", "Token", true, false);
+        let ref = ask_parameter("deploy_ref", "Ref (Branch, Tag, Commit)", true);
+        let task = ask_parameter("deploy_task", "Task", true);
+        let payload = ask_parameter("deploy_payload", "Payload (in JSON)", true, true);
+        let data = {
+            ref: ref,
+            auto_merge: false,
+            task: task,
+            required_contexts: [],
+            description: "Created by Github Deployments and Repository Dispatch Trigger",
+            payload: payload
+        };
+        GM_xmlhttpRequest({
+            method: "POST",
+            url: "https://api.github.com/repos/" + pathArray[1] + "/" + pathArray[2] + "/deployments",
+            headers: {"Authorization": "token " + token, "Accept": "application/vnd.github.ant-man-preview+json"},
+            data: JSON.stringify(data),
+            onload: (resp) => {
+                if (resp.status == 201) {
+                    console.log("Create deployments succeed: " + resp.responseText);
+                    window.alert("Create deployments succeed" + (resp.responseText?(": " + resp.responseText):""));
+                } else {
+                    var message = resp.responseText;
+                    try {
+                        message = JSON.parse(message).message;
+                    } catch (e) {}
+                    console.log("Create deployments failed: " + resp.responseText);
+                    window.alert("Create deployments failed: " + message);
+                }
+            },
+            onerror: (e) => {
+                console.log("Create deployments failed: " + e);
+                window.alert("Create deployments failed: " + e.message);
+            }
+        });
+    }
+
+    $(document).ready(() => {
+        let repo_dis_btn = $("<a class='btn btn-sm'>Repo Dispatch</a>").on("click", trigger_repo_dispatch);
+        let deployments_btn = $("<a class='btn btn-sm'>Deploy</a>").on("click", trigger_deployments);
+        $(".pagehead-actions").eq(0).prepend($("<li></li>").append(repo_dis_btn)).prepend($("<li></li>").append(deployments_btn));
     });
 
 })();
